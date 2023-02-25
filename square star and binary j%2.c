2f5c4417ffa76d9cf6662685a0f3bbca6e491404
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-02-25 11:00:28 -0800

    Add files via upload

---
+++ square star and binary j%2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if(i%2!=0){
+                printf("* ");
+            }else if(j==1||j==n){
+                printf("* ");
+            }else{
+                printf("%d ",j%2);
+            }
+        }printf("\n");
+    }return 0;
+}
+
