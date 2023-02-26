4a5e8af1386911ec5bdf7dfcc8b844a520f20baa
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-02-26 06:23:39 -0800

    Add files via upload

---
+++ triangle pattern i%2,border star , rest j.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            if(i%2==0) printf("* ");
+            else if(j==1||j==i) printf("* ");
+            else printf("%d ",j);
+        }printf("\n");
+    }return 0;
+}
